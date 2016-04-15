95b1822b25776b62d0961197274174d0c8369da5
utpal <utpaldps@gmail.com>
2016-04-15 15:50:09 +0530

    General node class

---
+++ generalNode.py
@@ -0,0 +1,22 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from networkx import *
+import signal
+import json
+import sys
+import Queue
+from collections import defaultdict
+from node import *
+
+class generalNode(Node):
+    def __init__(self):
+        self.startInterruptHandling()
+        print "binding on", sys.argv[1]
+        self.bind_receive('127.0.0.1',int(sys.argv[1]))
+        self.start_listening()
+
+def main():
+    G = generalNode()
+
+if __name__ == '__main__':
+    main()
